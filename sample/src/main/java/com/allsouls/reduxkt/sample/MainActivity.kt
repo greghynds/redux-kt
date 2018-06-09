aee2b4a766ac1dadae25bf3ae4dfe60f0dddff37
allsouls <allsoulscode@gmail.com>
2018-06-09 22:00:27 +0100

    Added lib and sample modules.

---
+++ sample/src/main/java/com/allsouls/reduxkt/sample/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.allsouls.reduxkt.sample
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.allsouls.reduxkt.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
