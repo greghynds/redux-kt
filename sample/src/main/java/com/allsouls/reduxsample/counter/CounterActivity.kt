8adc9470673e1d651170d0d17e343bd3076335c6
allsouls <allsoulscode@gmail.com>
2018-06-10 23:07:59 +0100

    Added utils package.

--- sample/src/main/java/com/allsouls/reduxsample/counter/CounterActivity.kt
+++ sample/src/main/java/com/allsouls/reduxsample/counter/CounterActivity.kt
@@ -3,6 +3,7 @@ package com.allsouls.reduxsample.counter
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.allsouls.redux.createStore
+import com.allsouls.redux.utils.connect
 import com.allsouls.reduxsample.R
 import kotlinx.android.synthetic.main.activity_sample.*
 
@@ -18,6 +19,8 @@ class CounterActivity : AppCompatActivity() {
         decrement_button.setOnClickListener { store.dispatch(createDecrementAction()) }
         reset_button.setOnClickListener { store.dispatch(createResetAction()) }
 
-        store.updates.subscribe { count -> count_text.text = getString(R.string.count_text_prefix, count) }
+        connect(store).subscribe { count ->
+            count_text.text = getString(R.string.count_text_prefix, count)
+        }
     }
 }
