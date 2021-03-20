60ffab367eb4a63d3d781e45e52813fbc1db4662
Greg Hynds <greg.hynds@bbc.co.uk>
2021-03-20 01:47:08 +0000

    Replace connect() extension for scoping updates to lifecycle

--- lib/src/main/java/com/allsouls/redux/utils/ReduxUtils.kt
+++ lib/src/main/java/com/allsouls/redux/utils/ReduxUtils.kt
@@ -1,19 +1,36 @@
 package com.allsouls.redux.utils
 
 import android.support.v7.app.AppCompatActivity
+import com.allsouls.redux.Action
+import com.allsouls.redux.Middleware
+import com.allsouls.redux.Reducer
 import com.allsouls.redux.Store
-import io.reactivex.disposables.Disposable
+import com.allsouls.redux.utils.RxActivity.Companion.ON_DESTROY
+import com.allsouls.redux.utils.RxActivity.Companion.ON_START
+import io.reactivex.Observable
 
 /**
- * Subscribes to updates from the store once the Activity has launched
- * and unsubscribes when the Activity is destroyed.
+ * Creates a store scoped to the Activity lifecycle.
  *
- * Calls the update function each time the store is updated.
+ * The store will not publish updates before the Activity has started
+ * or after the Activity has been destroyed.
  */
-fun <S> AppCompatActivity.connect(store: Store<S>, update: (S) -> Unit, error: (Throwable) -> Unit = {}): Disposable =
-        with(RxActivity.lifecycle(this)) {
-            filter { lifecycleEvent -> lifecycleEvent == RxActivity.ON_START }
-                    .takeUntil(filter { lifecycleEvent -> lifecycleEvent == RxActivity.ON_DESTROY })
-                    .concatMap { store.updates }
-                    .subscribe(update, error)
-        }
+fun <S> AppCompatActivity.createStore(
+    reducer: Reducer<S>,
+    initialState: S,
+    vararg middlewares: Middleware<S>
+): Store<S> {
+    val store = com.allsouls.redux.createStore(reducer, initialState, *middlewares)
+    val updates = with(RxActivity.lifecycle(this)) {
+        filter { lifecycleEvent -> lifecycleEvent == ON_START }
+            .takeUntil(filter { lifecycleEvent -> lifecycleEvent == ON_DESTROY })
+            .concatMap { store.updates }
+            .share()
+    }
+
+    return object : Store<S> {
+        override val dispatch: (Action) -> Action = store.dispatch
+        override val state: S get() = store.state
+        override val updates: Observable<S> get() = updates
+    }
+}
--- sample/src/main/java/com/allsouls/reduxsample/counter/CounterActivity.kt
+++ sample/src/main/java/com/allsouls/reduxsample/counter/CounterActivity.kt
@@ -1,17 +1,19 @@
 package com.allsouls.reduxsample.counter
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.allsouls.redux.createStore
-import com.allsouls.redux.utils.connect
+import com.allsouls.redux.Store
+import com.allsouls.redux.utils.createStore
 import com.allsouls.reduxsample.R
 import kotlinx.android.synthetic.main.activity_sample.*
 
 class CounterActivity : AppCompatActivity() {
 
     // create the store with the root reducer and an initial count
-    val store = createStore(counterReducer, initialState = 0)
+    private val store: Store<Int> by lazy { createStore(counterReducer, 0, logging) }
 
+    @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sample)
@@ -22,8 +24,8 @@ class CounterActivity : AppCompatActivity() {
         reset_button.setOnClickListener { store.dispatch(createResetAction()) }
 
         // subscribe to updates from the store
-        connect(store, update = { state ->
+        store.updates.subscribe { state ->
             count_text.text = getString(R.string.count_text_prefix, state)
-        })
+        }
     }
 }
