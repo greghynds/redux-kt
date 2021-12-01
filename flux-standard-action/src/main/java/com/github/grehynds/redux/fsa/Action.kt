fe9329d8027247fda828a6c6fb2fe7baa2482590
Greg Hynds <greg.hynds@tesco.com>
2021-12-01 11:14:19 +0000

    Rename 'lib' module to 'redux'

---
+++ flux-standard-action/src/main/java/com/github/grehynds/redux/fsa/Action.kt
@@ -0,0 +1,19 @@
+package com.github.grehynds.redux.fsa
+
+/**
+ * From the reduxjs docs:
+ * Actions are payloads of information that send data from your application to your store.
+ *
+ * Based on Flux Standard Action - https://github.com/redux-utilities/flux-standard-action
+ */
+data class Action(
+    val type: String = "",
+    val payload: Any? = null,
+    val error: Boolean = false
+) {
+    fun isOfType(vararg type: String): Boolean = type.contains(this.type)
+
+    companion object {
+        val EMPTY = Action("NONE")
+    }
+}
