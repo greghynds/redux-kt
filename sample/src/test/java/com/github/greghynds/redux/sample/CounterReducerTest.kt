df0f95c8f60ef98102109ae54659d01a94fefa87
Greg Hynds <gregwhynds@gmail.com>
2021-12-08 21:58:41 +0000

    Fix counter reducer tests

--- sample/src/test/java/com/github/greghynds/redux/sample/CounterReducerTest.kt
+++ sample/src/test/java/com/github/greghynds/redux/sample/CounterReducerTest.kt
@@ -2,27 +2,21 @@ package com.github.greghynds.redux.sample
 
 import junit.framework.Assert.assertEquals
 import org.junit.Test
-import com.github.greghynds.redux.Action
 
 class CounterReducerTest {
 
     @Test
-    fun `empty action returns initial state`() {
-        assertEquals(counterReducer(0, Action.EMPTY), 0)
-    }
-
-    @Test
     fun `increment action increments count`() {
-        assertEquals(counterReducer(0, createIncrementAction()), 1)
+        assertEquals(counterReducer(0, Increment()), 1)
     }
 
     @Test
     fun `decrement action decrements count`() {
-        assertEquals(counterReducer(1, createDecrementAction()), 0)
+        assertEquals(counterReducer(1, Decrement()), 0)
     }
 
     @Test
     fun `reset action sets count to zero`() {
-        assertEquals(counterReducer(1, createDecrementAction()), 0)
+        assertEquals(counterReducer(5, Reset()), 0)
     }
 }
