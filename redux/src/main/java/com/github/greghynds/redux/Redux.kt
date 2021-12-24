f896a9534f8fc0a781dbd73bc680a1893876aea8
Greg Hynds <60611327+greghynds@users.noreply.github.com>
2021-12-24 17:27:13 +0000

    Separate Android module from Kotlin library

--- redux/src/main/java/com/github/greghynds/redux/Redux.kt
+++ redux/src/main/java/com/github/greghynds/redux/Redux.kt
@@ -1,8 +1,5 @@
 package com.github.greghynds.redux
 
-import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Lifecycle
 import io.reactivex.Observable
 import io.reactivex.disposables.Disposable
 import io.reactivex.functions.Consumer
@@ -61,7 +58,7 @@ internal class Redux<State> {
      * and subscribe to changes.
      */
     val createStore: (Reducer<State>, State, StoreEnhancer<State>?) -> Store<State>
-        @SuppressLint("CheckResult")
+        @Suppress("CheckResult")
         get() = { reducer, initialState, enhancer ->
             var currentState = initialState
             val actions = BehaviorSubject.create<Any>()
@@ -146,28 +143,6 @@ fun <State> createStore(
 ): Store<State> {
     return with(Redux<State>()) {
         createStore(reducer, initialState, applyMiddleware(middlewares.asList()))
-    }
-}
-
-/**
- * Creates a store scoped to the Activity lifecycle.
- *
- * The store will not emit updates after the Activity has been destroyed.
- */
-fun <S> AppCompatActivity.createStore(
-    reducer: Reducer<S>,
-    initialState: S,
-    vararg middlewares: Middleware<S>
-): Store<S> {
-    val store = com.github.greghynds.redux.createStore(reducer, initialState, *middlewares)
-    val updates = store.updates
-        .filter { _: S -> lifecycle.currentState != Lifecycle.State.DESTROYED }
-        .share()
-
-    return object : Store<S> {
-        override val dispatch: (Any) -> Any = store.dispatch
-        override val state: S get() = store.state
-        override val updates: Observable<S> get() = updates
     }
 }
 
