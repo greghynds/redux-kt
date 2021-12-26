f9a8a8c34a0d268f09f888cbfb7778169ba30c03
Greg Hynds <gregwhynds@gmail.com>
2021-12-26 02:05:45 +0000

    Remove kotlin-android-extensions plugin

--- android-sample/src/main/java/com/github/greghynds/redux/sample/CounterActivity.kt
+++ android-sample/src/main/java/com/github/greghynds/redux/sample/CounterActivity.kt
@@ -2,10 +2,11 @@ package com.github.greghynds.redux.sample
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.github.greghynds.redux.Store
 import com.github.grehynds.redux.android.createStore
-import kotlinx.android.synthetic.main.activity_sample.*
 
 class CounterActivity : AppCompatActivity() {
 
@@ -18,13 +19,15 @@ class CounterActivity : AppCompatActivity() {
         setContentView(R.layout.activity_sample)
 
         // bind actions
-        increment_button.setOnClickListener { store.dispatch(Increment()) }
-        decrement_button.setOnClickListener { store.dispatch(Decrement()) }
-        reset_button.setOnClickListener { store.dispatch(Reset()) }
+        findViewById<Button>(R.id.increment_button).setOnClickListener { store.dispatch(Increment()) }
+        findViewById<Button>(R.id.decrement_button).setOnClickListener { store.dispatch(Decrement()) }
+        findViewById<Button>(R.id.reset_button).setOnClickListener { store.dispatch(Reset()) }
+
+        val countText = findViewById<TextView>(R.id.count_text)
 
         // subscribe to updates from the store
         store.subscribe { state ->
-            count_text.text = getString(R.string.count_text_prefix, state)
+            countText.text = getString(R.string.count_text_prefix, state)
         }
     }
 }
